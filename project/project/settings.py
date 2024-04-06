3e8f63345b27d67a35a5593077656702a9ed4ddc
DamirKFU <shabievdg@yandexlyceum.ru>
2024-04-06 22:30:38 +0300

    fix: fix django-test

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,8 +74,8 @@ DATABASES = {
         "NAME": os.getenv("DJANGO_DATABASE_NAME"),
         "USER": os.getenv("DJANGO_DATABASE_USER"),
         "PASSWORD": os.getenv("DJANGO_DATABASE_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "",
+        "HOST": "postgres",
+        "PORT": "5432",
     },
 }
 
